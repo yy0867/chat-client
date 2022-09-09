ce074788263e9f73d489b46cfdda9ec27d909ade
김세영 <yy0867@gmail.com>
2022-09-09 23:27:09 +0900

    init project

---
+++ Chatting/Chatting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatting
+//
+//  Created by 김세영 on 2022/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
